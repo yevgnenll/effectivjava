1f9afa14ccc3df1a23f2cc09aba8855b6910448b
seungkwon <yevngnenll@zuminternet.com>
2016-11-30 18:03:14 +0900

    parameter가 필요한 annotation 사용 예시

---
+++ src/Sample2.java
@@ -0,0 +1,18 @@
+/**
+ * Created by yevgnen on 2016-11-30.
+ */
+public class Sample2 {
+  @ExceptionTest(ArithmeticException.class)
+  public static void m1(){
+    int i = 0;
+    i = i / i;
+  }
+  @ExceptionTest(ArithmeticException.class)
+  public static void m2(){
+    int[] a = new int[0];
+    int i = a[1];
+  }
+
+  @ExceptionTest(ArithmeticException.class)
+  public static void m3(){}
+}
