0fcdb9fa27b1644485f12d373f98f2b112b36dd3
seungkwon <yevngnenll@zuminternet.com>
2016-11-30 11:50:26 +0900

    package 수정

--- src/effective/inner/TestInner.java
+++ src/effective/inner/TestInner.java
@@ -5,12 +5,18 @@ public class TestInner {
   int a = 10;
   public static void method(){
     System.out.println("inner method");
+
     class LocalClass{
       public void localPrint(){
         System.out.println("local class print");
       }
     }
   }
+  public static void main(String args[]){
+    System.out.println(b);
+  }
+
+  private static int b = 255;
 
   public class Inner{
     public void prnt(){
