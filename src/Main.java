c649af00fc6281d70cc0f97f4f551e80ab318138
seungkwon <yevngnenll@zuminternet.com>
2016-11-30 17:07:05 +0900

    refactorring
    enum 상수 안에 있는 값을 한꺼번에 보내어
    코드를 더욱 깔끔하게 만드는 방법

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,18 @@
-import java.util.DoubleSummaryStatistics;
+import java.util.Arrays;
+import java.util.Collection;
 
 public class Main {
 
   public static void main(String[] args) {
     double x = Double.parseDouble(String.valueOf(100));
     double y = Double.parseDouble(String.valueOf(7));
-    test(ExtendedOperation.class, x, y);
+    test(Arrays.asList(ExtendedOperation.values()), x, y);
     System.out.println(ExtendedOperation.class);
   }
 
-  private static <T extends Enum<T> & Operation> void test(
-      Class<T> opSet, double x, double y ){
-    for(Operation op : opSet.getEnumConstants())
+  private static void test(Collection<? extends Operation> opSet,
+      double x, double y){
+    for(Operation op : opSet)
       System.out.printf("%f %s %f = %f%n", x, op, y, op.apply(x, y));
   }
 
