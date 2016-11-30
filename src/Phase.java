2df3075259f1a5ee54fe25a94b95018330739d4e
seungkwon <yevngnenll@zuminternet.com>
2016-11-30 16:19:34 +0900

    refactorring
    배열 -> EnumMap 사용

--- src/Phase.java
+++ src/Phase.java
@@ -1,17 +1,33 @@
+import java.util.EnumMap;
+import java.util.Map;
+
 public enum Phase {
   SOLID, LIQUID, GAS;
 
-  public enum Transition{
-    MELT, FREEZE, BOIL, CONDENSE, SUBLIME, DEPOSIT;
+  public enum Transition {
+    MELT(SOLID, LIQUID), FREEZE(LIQUID, SOLID),
+    BOIL(LIQUID, GAS), CONDENSE(GAS, LIQUID),
+    SUBLIME(SOLID, GAS), DEPOSIT(GAS, SOLID);
 
-    private static final Transition[][] TRANSITIONS = {
-        { null, MELT, SUBLIME },
-        { FREEZE, null, BOIL },
-        { DEPOSIT, CONDENSE, null }
-    };
+    private final Phase src;
+    private final Phase dst;
 
-    public static Transition from(Phase src, Phase dst){
-      return TRANSITIONS[src.ordinal()][dst.ordinal()];
+    Transition(Phase src, Phase dst) {
+      this.src = src;
+      this.dst = dst;
+    }
+
+    private static final Map<Phase, Map<Phase, Transition>> m = new EnumMap<Phase, Map<Phase, Transition>>(Phase.class);
+
+    static {
+      for(Phase p : Phase.values())
+        m.put(p, new EnumMap<Phase, Transition>(Phase.class));
+      for(Transition trans : Transition.values())
+        m.get(trans.src).put(trans.dst, trans);
+    }
+
+    public static Transition from(Phase src, Phase dst) {
+      return m.get(src).get(dst);
     }
   }
 }
