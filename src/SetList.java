7b94821ecdbe5e1737bcbd7308fb5a31bf84f51b
seungkwon <yevngnenll@zuminternet.com>
2016-12-01 18:43:13 +0900

    overloading시 중의 해야함

---
+++ src/SetList.java
@@ -0,0 +1,25 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
+import java.util.TreeSet;
+
+/**
+ * Created by yevgnen on 2016-12-01.
+ */
+public class SetList {
+  public static void main(String[] args){
+    Set<Integer> set = new TreeSet<>();
+    List<Integer> list = new ArrayList<>();
+
+    for(int i = -3; i< 3; i++){
+      set.add(i);
+      list.add(i);
+    }
+    for(int i=0; i<3; i++){
+      set.remove(i);
+      list.remove((Integer) i);
+      System.out.println("list " + list);
+
+    }
+  }
+}
